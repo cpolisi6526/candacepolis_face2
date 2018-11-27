d1b7a180da6cf7f58fd17de829a511a5d983e489
BT_1N3_26 <cpolisi6526@bths.edu>
2018-11-27 15:18:09 -0500

    Initial commit

---
+++ src/faceDrawing.java
@@ -0,0 +1,32 @@
+public class faceDrawing {
+    private String[][] faceFeat;
+
+    public faceDrawing(String[][] faceFeat) {
+        this.faceFeat = faceFeat;
+    }
+
+    public void fill(String str) {
+        for (int col = 0; col < faceFeat[col].length; col++) {
+            for (int row = 0; row < faceFeat.length; row++) {
+                faceFeat[row][col] = str;
+            }
+        }
+    }
+
+    public void edit(String replace, int row, int col) {
+        faceFeat[row][col] = replace;
+    }
+
+    public String toString() {
+        String face = "";
+        for (int col = 0; col < faceFeat[col].length; col++) {
+            for (int row = 0; row < faceFeat.length; row++) {
+                face = face + faceFeat[row][col] + " ";
+            }
+            face = face + "\n";
+        }
+        return face;
+    }
+}
+
+
